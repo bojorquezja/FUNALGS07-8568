1ca8b9be41641c589bdfbe331d90a518f5545221
Fabri-Programmer <fabrizziopks@hotmail.com>
2024-10-03 08:49:36 -0500

    fertrolo

--- Prob13/Program.cs
+++ Prob13/Program.cs
@@ -1,39 +1,48 @@
-﻿int n, diasMes, empleados1 = 0, empleados2 = 0;
-double totalSueldos = 0;
+﻿int Cantidad, DM, M1 = 0, M2 = 0;
+double SueldoTotal = 0;
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("Ingrese la cantidad de empleados = ");
+Cantidad = int.Parse(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Ingrese la cantidad de días del mes = ");
+DM = int.Parse(Console.ReadLine());
+for (int i = 0; i < Cantidad; i++) //inicializacion
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"Numero de empleados =  {i + 1}:");
+    Console.Write("Nombre = ");
+    string Name = Console.ReadLine();
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.Write("Días que falto = ");
+    int DiaF = int.Parse(Console.ReadLine());
 
-Console.Write("Ingrese la cantidad de empleados: ");
-n = int.Parse(Console.ReadLine());
+    Console.Write("Salario mensual = ");
+    double SM = double.Parse(Console.ReadLine());
 
-Console.Write("Ingrese la cantidad de días del mes: ");
-diasMes = int.Parse(Console.ReadLine());
+    double SD = SM / DM;
+    double SF = SM - (DiaF * SD);
 
-for (int i = 0; i < n; i++) {
-    Console.WriteLine($"\nEmpleado {i + 1}:");
-    Console.Write("Nombre: ");
-    string nombre = Console.ReadLine();
+    SueldoTotal += SF;
 
-    Console.Write("Días que faltó: ");
-    int diasFaltados = int.Parse(Console.ReadLine());
-
-    Console.Write("Salario mensual: ");
-    double salarioMensual = double.Parse(Console.ReadLine());
-
-    double sueldoDiario = salarioMensual / diasMes;
-    double sueldoFinal = salarioMensual - (diasFaltados * sueldoDiario);
-
-    totalSueldos += sueldoFinal;
-
-    if (sueldoFinal >= 2500 && sueldoFinal <= 3500) {
-        empleados1++;
+    if (SF >= 2500 && SF <= 3500)
+    {
+        M1++;
     }
 
-    if (sueldoFinal < 1000 || sueldoFinal > 4000) {
-        empleados2++;
+    if (SF < 1000 || SF > 4000)
+    {
+        M2++;
     }
-
-    Console.WriteLine($"Sueldo final de {nombre}: {sueldoFinal:C2}");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Sueldo final = " + SD);
+    Console.WriteLine("\t******************");
+    Console.WriteLine("Tu nombre es = " + Name);
+    Console.WriteLine("\t******************");
 }
 
-Console.WriteLine($"\nMonto total de sueldos: {totalSueldos:C2}");
-Console.WriteLine($"Empleados que ganan entre 2500 y 3500: {empleados1}");
-Console.WriteLine($"Empleados que ganan menos de 1000 o más de 4000: {empleados2}");
+Console.WriteLine("Monto total de sueldos = " + SueldoTotal);
+Console.WriteLine("\t**************************************");
+Console.WriteLine("Empleados que ganan entre 2500 y 3500 = " + M1);
+Console.WriteLine("\t**************************************");
+Console.WriteLine("Empleados que ganan menos de 1000 o mas de 4000: = " + M2);
+Console.WriteLine("\t**************************************");
